3c3d0d7fe120d5534ceab86a8558d3540ecebbd9
zSarcoz <cesar.cottua@gmail.com>
2022-07-19 16:08:45 -0400

    components update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
@@ -12,24 +11,11 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          
+           
         </Switch>
       </div>
     </BrowserRouter>
   </>
-=======
-import "./App.css";
-import NavBar from "./components/NavBar";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        {/* <Home /> */}
-      </div>
-    </BrowserRouter>
->>>>>>> c27af2bbd66c2f9cdcf0851691dc6378f8e5b05b
   );
 }
 
