33b79f65ec4f801c8b7984f323d47be636d926dd
Sebas_montilla <sebastianam127@gmail.com>
2022-07-19 15:34:30 -0400

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <Home /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
