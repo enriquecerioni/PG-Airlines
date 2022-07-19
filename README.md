79030839bf972da10eb0aa6c04d945ce9e69927e
Enrique Cerioni <enriquecerioni39@gmail.com>
2022-07-19 11:07:33 -0300

    Initial commit GP

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# PG-Airlines
-PG Airlines Ecommerce.
+
+
+# Grupal Project - Airlines
+
+<!-- <p align="right">
+  <img height="200" src="./videogame.png" />
+</p> -->
+
+Airlines Ecommerce
---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001/'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
