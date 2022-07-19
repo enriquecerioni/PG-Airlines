7ed4a8ca707802e631792707974cb96b24a30f88
zSarcoz <cesar.cottua@gmail.com>
2022-07-19 15:34:13 -0400

    home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </>
   );
 }
 
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import s from "./styles/Home.module.css";
+
+export default function Home() {
+
+  return (
+    <div className={s.Home}>
+      {/* <div className={s.divImg}>
+        <img
+          className={s.img}
+          src={
+            "https://teelindy.com/wp-content/uploads/2019/03/default_image.png"
+          }
+          alt=""
+        />
+      </div> */}
+      <div className={s.container}>
+        <h1 className={s.title}>
+          Showcase your thought provoking topics and ideas
+        </h1>
+        <p className={s.text}>
+          Big company announcement or simple sub-header taking two or more
+          lines.
+        </p>
+        <Link to="/login">
+          <button className={s.btn}>Button</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
