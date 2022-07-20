86a01f9a4500144c94172ee446e171204ed9a764
AntonioMelini <antoniomelini@hotmail.com>
2022-07-20 15:06:45 -0300

    se agrega la coneccion con firebase

---
+++ api/src/db_flight/upload_db.js
@@ -0,0 +1,40 @@
+async function migrar(){
+
+// const initializeApp = require("firebase/app");
+   const firebase = require('firebase')
+   const admin = require('firebase-admin');
+   const data = require("./db.json");
+   const serviceAccount = require("./key_service_account.json");
+   const collectionKey = "db"; //Name of the collection
+   admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount)
+   });
+   const firestore = admin.firestore();
+   const settings = {timestampsInSnapshots: true};
+   firestore.settings(settings);
+   if (data && (typeof data === "object")) {
+   Object.keys(data).forEach(docKey => {
+   firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+      console.log("Document " + docKey + " successfully written!");
+   }).catch((error) => {
+      console.error("Error writing document: ", error);
+   });
+   });
+   }
+
+    
+   const firebaseConfig = {
+      apiKey: "AIzaSyBGr8PQQDvTRK484636fOa1XJVhIJ0lmqA",
+      authDomain: "prueba-dcc65.firebaseapp.com",
+      projectId: "prueba-dcc65",
+      storageBucket: "prueba-dcc65.appspot.com",
+      messagingSenderId: "743031201286",
+      appId: "1:743031201286:web:5df37e5654d096731f2d87"
+    };
+   // firebaseConfig.initializeApp
+    firebase.initializeApp(firebaseConfig);
+    const dbFirestore=firebase.firestore();
+    dbFirestore.settings({timestampsInSnapshots: true})
+
+}
+exports.modules=migrar()
