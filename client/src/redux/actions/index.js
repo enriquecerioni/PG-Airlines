7eeeac1a55b87ed3c514c0983a4438b211018f36
Matias Valenti <51561485+MattValenti11@users.noreply.github.com>
2022-07-20 19:13:35 -0300

    json

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -4,8 +4,6 @@ export const FILTER_ALPHABETICALLY = "FILTER_ALPHABETICALLY"
 export const GET_FLIGHT_INFO = "GET_FLIGHT_INFO"
 export const GET_USER_INFO = "GET_USER_INFO"
 
-export const ORDER_PRICE = 'ORDER_PRICE'
-
 export const getAllFlights = () => {
     return async function (dispatch) {
        fetch('LINK')
@@ -24,8 +22,4 @@ export const getFlightInfo = () => {
 
 export const filterAlphabetically = (payload) => {
     return {type: FILTER_ALPHABETICALLY, payload}
-}
-
-export const orderByPrice = (payload) => {
-    return {type: ORDER_PRICE, payload}
 }
