9326a673741adf7e415f42f0c5753bc4291a3504
Enrique Cerioni <enriquecerioni39@gmail.com>
2022-07-20 17:15:25 -0300

    Create Folder Controller and Flight Controller

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { getAllFlight } = require('../controllers/FlightController');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,6 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get('/flights', getAllFlight);
 
 module.exports = router;
