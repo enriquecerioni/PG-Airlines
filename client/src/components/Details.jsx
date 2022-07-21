9738713d162fbc95b67d40f3122949ffc9c367f4
macarena <singhmacarena.r@gmail.com>
2022-07-21 19:37:05 -0300

    detail route update

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getAllFlights } from "../redux/actions/index.js";
+import { getFlightByID, cleanDetails } from "../redux/actions/index.js";
 import s from "./styles/Details.module.css";
 import { Link } from "react-router-dom";
 // import logo from './styles/logo.png'
@@ -9,55 +9,60 @@ import NavBar from "./NavBar.jsx";
 
 function Details() {
   const { id } = useParams();
-  // const dispatch = useDispatch();
-  const details = useSelector((state) => state.flights);
+  const dispatch = useDispatch();
+  const details = useSelector((state) => state.flight);
+  console.log(details)
 
-  const flightA = (item) => {
-    return item.flight === id;
-  };
-  const a = details.find(flightA);
-  console.log(a);
+  useEffect(() => {
+      dispatch(getFlightByID(id))
+    return () => {
+      dispatch(cleanDetails())
+    }
+  }, [])
 
-  // const detail = a.map(e => {
-  //   return e
-  // })
-  // console.log(detail)
   return (
     <div>
       <div className={s.container}>
+
         <Link className={s.links} to="/">
           <button className={s.btnHome}>Go to Home</button>
         </Link>
-        {a ? (
-          <div className={s.detail}>
-            <div className={s.divA}>
-              <img className={s.logo} src={a.logo} alt="Img" />
-              <div className={s.airline}>{a.airline}</div>
-              <div className={s.departureDate}>{a.departureDate}</div>
 
-              <div className={s.durationEstimated}>{a.durationEstimated}</div>
-              <button className={s.btnDrop}>˅</button>
+        { details ? details.map(d => {
+            return (
+          <div key={d.flight} className={s.detail}>
+            <div className={s.detail}>
+              <div className={s.divA}>
+                <img className={s.logo} src={d.logo} alt="Img" />
+                <div className={s.airline}>{d.airline}</div>
+                <div className={s.departureDate}>{d.departureDate}</div>
+
+                <div className={s.durationEstimated}>{d.durationEstimated}</div>
+                <button className={s.btnDrop}>˅</button>
+              </div>
+              <div className={s.info}>
+                <div className={s.arrivalHour}>{d.arrivalHour}</div>
+                <div className={s.arrivalDate}>{d.arrivalDate}</div>
+                <div className={s.departureHour}>{d.departureHour}</div>
+                <div className={s.description}>{d.description}</div>
+                <div className={s.destination}>{d.destination}</div>
+                <div className={s.origin}>{d.origin}</div>
+              </div> 
+
+              <div className={s.divPrices}>
+                <img className={s.logoPrice} src={details.logo} alt="Img" />
+                <div className={s.airlinePrice}>{details.airline}</div>
+                <div className={s.priceP}>{details.price}</div>
+                <button className={s.btn}>Reservar</button>
+              </div> 
             </div>
-            <div className={s.info}>
-              <div className={s.arrivalHour}>{a.arrivalHour}</div>
-              <div className={s.arrivalDate}>{a.arrivalDate}</div>
-              <div className={s.departureHour}>{a.departureHour}</div>
-              <div className={s.description}>{a.description}</div>
-              <div className={s.destination}>{a.destination}</div>
-              <div className={s.origin}>{a.origin}</div>
             </div>
-          </div>
-        ) : (
-          "no hay nada che"
+          ) 
+        }) 
+        : (
+          <h1>NADA</h1>
         )}
-        <div className={s.divPrices}>
-          <img className={s.logoPrice} src={a.logo} alt="Img" />
-          <div className={s.airlinePrice}>{a.airline}</div>
-          <div className={s.priceP}>${a.price}</div>
-          <Link className={s.links} to="/login">
-            <button className={s.btn}>Reservar</button>
-          </Link>
-        </div>
+
       </div>
     </div>
   );
