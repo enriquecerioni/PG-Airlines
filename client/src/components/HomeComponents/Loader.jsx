aceb25e6302e5cda5441bee6b932a72eef171dd1
zSarcoz <cesar.cottua@gmail.com>
2022-07-28 12:24:24 -0400

    Loader ready

--- client/src/components/HomeComponents/Loader.jsx
+++ client/src/components/HomeComponents/Loader.jsx
@@ -7,7 +7,7 @@ export default function loader(){
         <div className="fondo">
             <h2 className="mensaje">We're preparing your flights</h2>
             <h3>Please Wait</h3>
-            <div className="tainer">
+            <div className="tainer"> 
                 <div className="load"></div>
                 <p className="message">Loading...</p>
             </div>
