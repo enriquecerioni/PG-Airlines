ba6af26db35cac5db8f29c1907b228d179bb11c3
Matias Valenti <51561485+MattValenti11@users.noreply.github.com>
2022-07-28 20:37:59 -0300

    estilos de ordenamientos

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -3,6 +3,12 @@ import style from './styles/Display.module.css'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+
 const marks = [
     {value: 10, label: '< $20.000'},
     {value: 45, label: '$20.000 - $40.000'},
@@ -11,7 +17,7 @@ const marks = [
 ];
 
 // {handlePrice, handleAlph}
-function Filter({handlePrice, handleAlph, orderPriceSelect, orderAlpSelect, handleFilterPrice, airlinesData, handleClick, handleSearchAirlines}) {
+function Filter({handlePrice, handleAlph, orderPriceSelect, orderAlpSelect, handleFilterPrice, airlinesData, handleClick, handleSearchAirlines,orderByPrice,orderAlphabetically}) {
 
     function handleReset(e) {
         e.preventDefault();
@@ -66,19 +72,61 @@ function Filter({handlePrice, handleAlph, orderPriceSelect, orderAlpSelect, hand
         </div> 
         
         <h2>Order:</h2>
-        <select onChange={e => handlePrice(e)} ref={orderPriceSelect} >
+        {/* <select onChange={e => handlePrice(e)} ref={orderPriceSelect} >
             <option value='initial'>Price</option>
             <option value="high">High to low</option>
             <option value="low">Low to High</option>
-        </select>
+        </select> */}
 
-        <select onChange={e => handleAlph(e)} ref={orderAlpSelect} >
+        <div>
+            <FormControl sx={{ m: 1, minWidth: 250 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Price</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={orderByPrice}
+                    ref={orderPriceSelect}
+                    onChange={e => handlePrice(e)}
+                    autoWidth
+                    label="Price"
+                >
+                    <MenuItem value={"initial"}>Price</MenuItem>
+                    <MenuItem value={"high"}>High to low</MenuItem>
+                    <MenuItem value={"low"}>Low to High</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+
+        {/* <select onChange={e => handleAlph(e)} ref={orderAlpSelect} >
             <option value='initial'>Alphabet</option>
             <option value="asc">A - Z</option>
             <option value="dsc">Z - A</option>
-        </select>
+        </select> */}
+          <div>
+            <FormControl sx={{ m: 1, minWidth: 250 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Alphabet</InputLabel>
+                <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={orderAlphabetically}
+                    onChange={e => handleAlph(e)}
+                    ref={orderAlpSelect}
+                    autoWidth
+                    label="Alphabet"
+                >
+                    <MenuItem value='initial'>Alphabet</MenuItem>
+                    <MenuItem value="asc">A - Z</MenuItem>
+                    <MenuItem value="dsc">Z - A</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
 
-        <button onClick={handleReset}>RESET FILTER</button>
+        {/* <button onClick={handleReset}>RESET FILTER</button> */}
+        <Box sx={{ '& button': { m: 1 } }}>
+            <Button variant="contained" size="medium" onClick={handleReset}>
+                RESET FILTER
+            </Button>
+     </Box> 
     </div>
   )
 }
