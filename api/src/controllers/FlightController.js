4b1f01265b12fdb1a451dde2d929a1db5f68bd38
unknown <choymedina1989@gmail.com>
2022-08-01 02:14:18 -0500

    feat(client/): post and put created

--- api/src/controllers/FlightController.js
+++ api/src/controllers/FlightController.js
@@ -1,5 +1,4 @@
 const firebase = require("firebase");
-const { Flights } = require("../db");
 
 const getAllFlight = async (req, res) => {
   const dbFirestore = firebase.firestore();
@@ -20,45 +19,52 @@ const getAllFlight = async (req, res) => {
   }
 };
 
-async function getOriginFlight(req,res){
+async function getOriginFlight(req, res) {
   try {
-    const {origen}= req.query;
+    const { origen } = req.query;
     const dbFirestore = firebase.firestore();
-    
-    if(!origen)res.send("write a origin")
-    else{
-    let vuelos = await dbFirestore.collection("db").get()
-    let arrayDatos= vuelos.docs.filter(element => element.data().origin.toLowerCase().includes(origen.toLowerCase()));
-    let vuelosFiltrados = arrayDatos.map(arr => arr._delegate._document.data.value.mapValue.fields);
+
+    if (!origen) res.send("write a origin")
+    else {
+      let vuelos = await dbFirestore.collection("db").get()
+      let arrayDatos = vuelos.docs.filter(element => element.data().origin.toLowerCase().includes(origen.toLowerCase()));
+      let vuelosFiltrados = arrayDatos.map(arr => arr._delegate._document.data.value.mapValue.fields);
       vuelosFiltrados.length ? res.send(vuelosFiltrados) : res.send("Origin not found")
-    
-  }} catch (error) {
+
+    }
+  } catch (error) {
     console.log(error)
   }
 }
 
 async function updateToflights(req, res) {
   try {
-     //const { flight, airline, logo, price, stock, origin, duration, depH, arrH, destination, depD, arrD, description } = req.body;
-    const { flight} = req.body;
-    console.log(flight);
-  
-   // console.log(req.body);
-   // console.log(airline,flight);
+    const { flight } = req.body;
+    console.log(flight)
     if (!flight.id == ' ') {
-      // let Flight = await Flights.db(
-      //   {
-      //     permissions: true,
-      //   },
-      //   {
-      //     where: { email: email },
-      //   }
-      // );
-        if(false){
-            res.status(404).send("Flight not Found");
-        }else{
-            res.send("Flight edited.")
-        }
+      const dbFirestore = firebase.firestore();
+    
+      let flightEdit = await dbFirestore.collection("db").doc(flight.id).update(
+        {
+          airline: flight.airline,
+          arrivalDate: flight.arrivalDate,
+          arrivalHour: flight.arrivalHour,
+          departureDate: flight.departureDate,
+          departureHour: flight.departureHour,
+          description: flight.description,
+          destination: flight.destination,
+          durationEstimated: flight.duration,
+          flight: flight.flight,
+          logo: flight.logo,
+          origin: flight.origin,
+          price: flight.price,
+          stock: flight.stock
+        });
+      if (flightEdit[0] === 0) {
+        res.status(404).send("Flight not Found");
+      } else {
+        res.send("Flight edited.")
+      }
     }
   } catch (error) {
     console.log(error);
@@ -66,42 +72,68 @@ async function updateToflights(req, res) {
 }
 
 
-async function createUser(req, res) {
-  const { flight} = req.body;
+async function createFlights(req, res) {
+  const { flight } = req.body;
+  console.log(flight);
   try {
-    if (!flight.id == ' ') {
-      let flight = await Flights.findAll({
-        where: { flight: flight.id },
-      });
-      if (flight.length) {
-      } 
+    if (true) {
+      const dbFirestore = firebase.firestore();
+      let flightdb = await dbFirestore.collection("db").doc(flight.id);
+      if (false) {
+      }
       else {
-        let flightCreate = await Flights.create({
-          airline: "LATAM Airlines Group",
-          arrivalDate: "2022-12-13",
-          arrivalHour: "15:05",
-          departureDate: "2022-12-12",
-          departureHour: "19:35",
-          description: "Detail",
-          destination: "Punta Cana",
-    durationEstimated: "20:30",
-              airline: "LA2368",
-                logo: "https://www.despegar.com.ar/flights-images/latest/common/airlines/25x25/LA.png",
-              origin: "Buenos Aires",
-               price: 300000,
-               stock: 98
-          
-             });
-        return res.status(201).json(userCreated);
+        let flightCreate = await flightdb.set(
+          {
+            airline: flight.airline,
+            arrivalDate: flight.arrivalDate,
+            arrivalHour: flight.arrivalHour,
+            departureDate: flight.departureDate,
+            departureHour: flight.departureHour,
+            description: flight.description,
+            destination: flight.destination,
+            durationEstimated: flight.duration,
+            flight: flight.flight,
+            logo: flight.logo,
+            origin: flight.origin,
+            price: flight.price,
+            stock: flight.stock
+
+          });
+       return res.status(201).json(flightCreate);
       }
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
+
+
+async function deleteFlights(req, res) {
+  const { flightIds } = req.body;
+ 
+  try {
+    if (!flightIds.leght > 0 ) {
+
+      const dbFirestore = firebase.firestore();
+      let flightdb
+      await flightIds.foreach(async (f) =>{
+           flightdb  = await dbFirestore.collection("db").doc(f).delete();
+      });          
+      if (false) {
+      }
+      else {        
+       return res.status(201).json(flightdb);
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
 module.exports = {
   getAllFlight,
   getOriginFlight,
   updateToflights,
-  createUser
+  createFlights,
+  deleteFlights
 };
