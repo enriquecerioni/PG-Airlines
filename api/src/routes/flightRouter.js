408f9333980c57099ae19df9acbdcde87f8382ab
unknown <choymedina1989@gmail.com>
2022-08-01 02:16:08 -0500

    feat(api/): routes post, put and delete

--- api/src/routes/flightRouter.js
+++ api/src/routes/flightRouter.js
@@ -1,12 +1,12 @@
-const { getAllFlight,getOriginFlight,updateToflights,createUser } = require('../controllers/FlightController');
+const { getAllFlight,getOriginFlight,updateToflights,createFlights,deleteFlights } = require('../controllers/FlightController');
 const { Router } = require('express');
 
 const flightRouter = Router();
 
-
 flightRouter.get('/',getAllFlight)
 flightRouter.get('/origen',getOriginFlight)
-flightRouter.post('/create',createUser)
-flightRouter.post('/update',updateToflights)
+flightRouter.post('/create',createFlights)
+flightRouter.put('/update',updateToflights)
+flightRouter.delete('/delete',deleteFlights)
 
 module.exports=flightRouter;
